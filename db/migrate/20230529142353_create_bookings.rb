b02e52fe4d5b14867931bfd2eb00a21563b3d113
Natalie Tsering <natalie.tsering@gmail.com>
2023-05-29 16:42:03 +0200

    updated the model booking

--- db/migrate/20230529142353_create_bookings.rb
+++ db/migrate/20230529142353_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.string :start_date
-      t.string :end_date
+      t.datetime :start_date
+      t.datetime :end_date
       t.string :status
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_142353) do
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "start_date"
-    t.string "end_date"
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
