c47592035c3e8bad82f1273f74ec64f72bf33593
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-05-30 12:19:10 +0200

    new/create method

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,13 @@
 class OffersController < ApplicationController
+  def create
+    @offers = Offer.create(offer_params)
+    @offer.save
+    redirect_to offers_path(@offers)
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :color, :length, :size, :style)
+  end
 end
