25070c531bc59b66e0c6d4cd9e7e60adc420de7a
Natalie Tsering <natalie.tsering@gmail.com>
2023-05-30 13:15:20 +0200

    booking details done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
   belongs_to :user
   belongs_to :offer
-  validates :size, inclusion: { in: ["reserved", "pending", "booked"], allow_nil: false }
+  validates :status, inclusion: { in: ["reserved", "pending", "booked"], allow_nil: false }
 end
